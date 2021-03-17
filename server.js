25895fe46bfc66350fc4088c3f2a6ac83e1db267
cknduo <chloeesilvester@gmail.com>
2021-03-17 15:26:20 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+let PORT = process.env.PORT || 3000
+let count = 0
+
+app.get('/count', (req, res) => {
+    console.log('Current count is: ' + count)
+    res.send({count})
+})
+
+app.get('/increase-count', (req, res) => {
+    count++
+    console.log('Increased count to: ' + count)
+    res.send({count})
+})
+
+app.get('/decrease-count', (req, res) => {
+    count--
+    console.log('Decreased count to: ' + count)
+    res.send({count})
+})
+
+app.use(express.static('static'))
+app.listen(PORT)
